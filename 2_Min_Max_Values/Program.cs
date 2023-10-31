d23336f8f839d0c54ba1806d1397dd3f30e58f2d
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-31 21:53:05 +0200

    Add project files.

---
+++ 2_Min_Max_Values/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _2_Min_Max_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int maxValue = int.MinValue;
+            int minValue = int.MaxValue;
+
+            if (n > numbers.Length)
+            {
+                n = numbers.Length;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (maxValue < currentNumber )
+                {
+                    maxValue = currentNumber;
+                }
+                if (minValue > currentNumber)
+                {
+                    minValue = currentNumber;
+                }
+            }
+
+            Console.WriteLine(maxValue);
+            Console.WriteLine(minValue);
+        }
+
+    }
+}
---
+++ ExamPrep_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamPrep_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
